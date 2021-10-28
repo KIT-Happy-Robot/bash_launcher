e7aa51e5134d11dc0417fa12b8bece2390657d58
iidaissei <issei.288.higashi@icloud.com>
2021-10-28 10:31:17 +0900

    Fix: file name の修正

--- executions/yumeko_inspection.bash
+++ executions/yumeko_inspection.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-tab="--tab-with-profile=roslaunch --command"
-window="--window-with-profile=roslaunch --command"
+tab="--tab-with-profile=bash_launcher --command"
+window="--window-with-profile=bash_launcher --command"
 
 cd $HOME/test_ws/src/bash_launcher/bashes
 
@@ -10,8 +10,8 @@ gnome-terminal \
     $tab 'bash -c "sleep 1.0 ; ./minimal.bash"'\
     $tab 'bash -c "sleep 1.2 ; ./sensor.bash"'\
     $tab 'bash -c "sleep 1.4 ; ./amcl.bash"'\
-    $tab 'bash -c "sleep 1.6 ; ./rviz_navi.bash"'\
-    $tab 'bash -c "sleep 1.6 ; ./voice_common.bash"'\
+    $tab 'bash -c "sleep 1.4 ; ./rviz_navi.bash"'\
+    # $tab 'bash -c "sleep 1.6 ; ./voice_common.bash"'\
 
 # マニピュレーションと画像処理
 gnome-terminal \
@@ -20,10 +20,10 @@ gnome-terminal \
     $tab 'bash -c "sleep 2.4 ; ./three_deimensional.bash"'\
     $tab 'bash -c "sleep 2.6 ; ./darknet_ylov4.bash"'\
 
-# 音声
-gnome-terminal \
-    $tab 'bash -c "sleep 3.0 ; ./voice_common.bash"'\
+# # 音声
+# gnome-terminal \
+#     $tab 'bash -c "sleep 3.0 ; ./voice_common.bash"'\
 
-# マスター関連
-gnome-terminal \
-    $tab 'bash -c "sleep 4.0 ; ./actplan_executor.bash"'\
+# # マスター関連
+# gnome-terminal \
+#     $tab 'bash -c "sleep 4.0 ; ./actplan_executor.bash"'\
