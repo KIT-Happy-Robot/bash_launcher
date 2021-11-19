f577d9c682988c821c863fa2fdccbe6050350fc3
HappyYusuke <c1005073@planet.kanazawa-it.ac.jp>
2021-11-19 17:36:53 +0900

    add chaser19.bash cml_launch.bash

---
+++ bashes/chaser19.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+rosrun chaser19 chaser19
---
+++ executions/cml_launch.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+tab="--tab-with-profile=bash_launcher --command"
+window="--window-with-profile=bash_launcher --command"
+
+cd $HOME/catkin_ws/src/bash_launcher/bashes
+
+gnome-terminal \
+    $tab 'bash -c "sleep 1.0 ; ./minimal.bash"'\
+    $tab 'bash -c "sleep 1.2 ; ./sensor.bash"'\
+    $tab 'bash -c "sleep 1.4 ; ./amcl.bash"'\
+    $tab 'bash -c "sleep 1.4 ; ./rviz_navi.bash"'\
+    $tab 'bash -c "sleep 1.6 ; ./voice_common.bash"'\
+    $tab 'bash -c "sleep 1.5 ; ./chaser19.bash"'\
